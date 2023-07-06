06c6a01b6d845ab42165037af85c13f1e99c023e
Documenter.jl <documenter@juliadocs.github.io>
2023-07-06 09:03:21 +0000

    build based on c662ffc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MortgageCalculators","category":"page"},{"location":"#MortgageCalculators","page":"Home","title":"MortgageCalculators","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MortgageCalculators.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MortgageCalculators]","category":"page"}]
+}
